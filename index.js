18c1a48d5f926a15f22a365e937e7843dd5a8ee1
Kyle Treman <jkt290@nyu.edu>
2018-04-15 22:39:35 -0400

    Add link to code

---
+++ index.js
@@ -0,0 +1,5 @@
+const viewTheCodeBtn = document.getElementById("github");
+
+viewTheCodeBtn.addEventListener("click", function() {
+  location.href = "https://github.com/jkyletreman/meal_planner_react"
+})

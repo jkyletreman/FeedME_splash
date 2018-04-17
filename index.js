8fc5793b38360b8c735173a0310a6022da260396
Kyle Treman <jkt290@nyu.edu>
2018-04-17 12:43:41 -0400

    Add app link

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const viewTheCodeBtn = document.getElementById("github");
 viewTheCodeBtn.addEventListener("click", function() {
   location.href = "https://github.com/jkyletreman/meal_planner_react"
 })
+
+const viewTheApp = document.getElementById("app");
+
+viewTheCodeBtn.addEventListener("click", function() {
+  location.href = "https://feedme-mealplanner.herokuapp.com/"
+})

4297c008e2b95baf54865cf814024d048b2c58ea
Kyle Treman <jkt290@nyu.edu>
2018-04-17 13:14:47 -0400

    Add link to personal site

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ const viewTheApp = document.getElementById("app");
 viewTheApp.addEventListener("click", function() {
   location.href = "https://feedme-mealplanner.herokuapp.com/"
 })
+
+const viewTheDev = document.getElementById("dev");
+
+viewTheDev.addEventListener("click", function() {
+  location.href = "https://jkyletreman.github.io/personal-site/"
+})
